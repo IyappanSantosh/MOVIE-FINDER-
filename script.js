1a98c60394193e09c77c311c3faab0ddf5dc7bd0
IYAPPAN S P <146472704+IyappanSantosh@users.noreply.github.com>
2025-03-04 18:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//http://img.omdbapi.com/?apikey=14e82aca&s=mummy
+http: document.addEventListener("DOMContentLoaded", () => {
+    const movieForm = document.getElementById("movieForm");
+    const movieResults = document.getElementById("movieResults");
+    movieForm.addEventListener("submit", (e) => {
+            const movieName = document.getElementById("movieInput").value;
+            e.preventDefault(); 
+            searchMovies(movieName);      //ethula vanthu erukum bcoz it is a function     
+    });
+    async function searchMovies(movieName){
+        
+        
+        try{
+            movieResults.innerHTML = '<div class="loading">Searching movies...</div>';
+            const response =await fetch(`http://www.omdbapi.com?apikey=14e82aca&s=${movieName}`);
+            const data = await response.json();
+            if((data.response === 'False')) {
+                throw new Error("query not found");
+            }
+            displayMovies(data.Search);
+        } catch (error){
+            movieResults.innerHTML =`
+            <div class="error-message">"An error occurred. Please try again later."
+            </div>
+            `;
+         }
+    }
+    function displayMovies(movies){
+        movieResults.innerHTML = `
+        <div class="movies-grid">
+        ${movies.map((movie) => `
+            <div class="movie-card">
+            <img 
+            src="${movie.Poster}" 
+            alt="${movie.Title}"
+            class="movie-poster"
+            />
+            <div class="movie-info">
+            <h3 class='movie-title'>${movie.Title}</h3>
+            <div class='movie-year'>${movie.Year}</div>
+            </div>
+            </div>
+           `
+        ).join("")}
+        </div>
+        `;
+    }
+});
+
+
+
+
+
+    
